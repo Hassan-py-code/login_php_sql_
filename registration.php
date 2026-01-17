0510a112d3fbe8fd1c137fb11fe2888565fc05dd
Hassan <hachehri@gmail.com>
2026-01-17 20:13:47 +0100

    add file php to add form user

---
+++ registration.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<div class="container">
+    
+    <?php   
+       if(isset($_POST["submit"])){
+
+          $fullname=$_POST["fullname"];
+          $email=$_POST["email"];
+          $password=$_POST["password"];
+          $passwordRepeat=$_POST["repeat_password"];
+
+          $error =array();
+          if(empty($fullname) or empty($email) or empty($password) or empty($passwordRepeat)){
+            array_push($error,"All fields are required");
+          }
+ 
+          if(!filter_var($email , FILTER_VALIDATE_EMAIL)){
+                  array_push($error, "email is not valid");
+          }
+         
+         if(strlen($password) < 8){
+              array_push($error , "Password must be at 8 chractes long");
+         }
+
+         if($password !==$passwordRepeat ){
+           array_push($error ,"password does not match");
+         }
+
+         if(count($error) > 0 )
+         foreach($error as $errors){
+          echo "<div class='alert alert-danger'> $errors  </div>";
+        }else{
+            
+        }
+       }
+    ?>
+
+    <form action="registration.php" method="post">
+
+            <div class="form-group">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+            </div>
+
+            <div class="form-group">
+                <input type="email" class="form-control" name="email" placeholder="Email:">
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password:">
+            </div>
+
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
+            </div>
+
+        </form>
+        <div>
+        <div><p>Already Registered <a href="login.php">Login Here</a></p></div>
+      </div>
+    </div>
+
+   
+</body>
+</html>
