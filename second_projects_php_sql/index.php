fd457020d18a465817a0ed23fdf675ddd4497863
Hassan <hachehri@gmail.com>
2026-01-23 15:57:19 +0100

    modify my projects and creat projects

---
+++ second_projects_php_sql/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projects</title>
+    <!-- link css -->
+     <link rel="stylesheet" href="style.css">
+    <!-- link freamworke -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+     
+     <div class="container">
+        <form action="#" method="post">
+          <div class="form_group">
+            <input type="text" name="first_name" placeholder="first name">
+          </div>
+
+           <div class="form_group">
+            <input type="text" name="last_name" placeholder="last name">
+          </div>
+
+           <div class="form_group">
+            <input type="email" name="email" placeholder="email">
+          </div>
+
+           <div class="form_group">
+                <select name="color" id="">
+                    <option value="">Choose color</option>
+                    <option value="red">red</option>
+                    <option value="green">green</option>
+                    <option value="blue">Blue</option>
+                </select>
+          </div>
+
+          <div class="form-group">
+             <input type="submit" value="submit" name="submit">
+          </div>
+
+          
+
+        </form>
+     </div>
+
+
+
+</body>
+</html>
